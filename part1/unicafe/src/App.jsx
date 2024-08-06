b8c27df9e9eddff03785e0d4021d27418faf6d82
azevedo7 <jazevedo0406@gmail.com>
2024-08-06 10:45:59 +0100

    unicafe: 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addOne = (value, setValue) => () => {setValue(value + 1)}
+
+  return(
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={addOne(good, setGood)} text={'good'}/>
+      <Button onClick={addOne(neutral, setNeutral)} text={'neutral'} />
+      <Button onClick={addOne(bad, setBad)} text={'bad'} />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>netural {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
   )
 }
 
