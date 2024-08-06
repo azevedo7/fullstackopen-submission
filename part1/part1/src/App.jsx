f680d232596628cb80965458942347d857e69fb4
azevedo7 <jazevedo0406@gmail.com>
2024-08-06 07:50:39 +0100

    Destructuring

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,17 +1,23 @@
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return(
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you are probably born in {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return(
     <div>
       <h1>Greetings</h1>
-      <Hello name="George"/>
-      <Hello name='Daisy'/> 
+      <Hello name={name} age={age} />
+      <Hello name='Daisy' age={10+24}/> 
     </div>
   )
 }
