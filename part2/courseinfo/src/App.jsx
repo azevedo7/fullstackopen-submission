a2becfdc01c827677d61dd8b784c3c6add74be5c
azevedo7 <jazevedo0406@gmail.com>
2024-08-06 14:11:33 +0100

    courseinfo: 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,58 @@
+const Header = ({ course }) => {
+  return (<h1>{course.name}</h1>)
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map(part => <Part key={part.name} part={part}/>)} 
+    </>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Total = ({ total }) => <p>Number of exercises {total.reduce((acc, curr) => acc + curr.exercises, 0)}</p>
+
+const Course = ({ course }) => {
+  return(
+    <>
+      <Header course={course}/>
+      <Content parts={course.parts}/>
+    </>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+
+export default App
