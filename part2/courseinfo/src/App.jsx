61a3105b104fd38e2a2e1c267e3715c896bc5056
azevedo7 <jazevedo0406@gmail.com>
2024-08-06 14:25:15 +0100

    courseinfo: 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -19,45 +19,67 @@ const Part = (props) => {
 
 const Total = ({ parts }) => <p><b>Total of {parts.reduce((acc, curr) => acc + curr.exercises, 0)} exercises</b></p>
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   return(
     <>
-      <Header course={course}/>
-      <Content parts={course.parts}/>
+      {courses.map(course => 
+        <div key={course.id}>
+          <Header course={course}/>
+          <Content parts={course.parts}/>
+        </div>
+      )}
     </>
   )
 }
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ] 
 
-  return <Course course={course} />
+  return <Course courses={courses} />
 }
 
 
