1b16f528b5d0835b5f2bfb0117d23e5ab7f15bbb
azevedo7 <jazevedo0406@gmail.com>
2024-08-07 11:47:46 +0100

    2.b: Getting data from server

--- part2/part2-notes-frontend-part2-1/src/App.jsx
+++ part2/part2-notes-frontend-part2-1/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+        console.log('promise')
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (e) => {
     e.preventDefault()
