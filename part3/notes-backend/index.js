15cff47b30c7f9c2e0f8ff6aa1e76fba063ba21a
azevedo7 <jazevedo0406@gmail.com>
2024-08-12 10:44:45 +0100

    Part 3: Render deployment

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
 
 let notes = [
     {
@@ -90,7 +92,7 @@ app.post('/api/notes', (req, res) => {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT ||3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/notes-frontend/src/App.jsx
@@ -0,0 +1,105 @@
+import { useState, useEffect } from 'react'
+import noteService from './services/notes'
+import Note from './components/Note'
+import Notification from './components/Notification'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const toggleImportance = (id) => {
+    const note = notes.find(note => note.id === id)
+    const changedNote = {...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(`the note '${note.content}' was already removed from server`)
+        setInterval(() => {
+          setErrorMessage(null) 
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+const Footer = () => {
+        const footerStyle = {
+            color: 'green',
+            fontStyle: 'italic',
+            fontSize: 16
+        }
+        return(
+            <div style={footerStyle}>
+                <br />
+                <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+            </div>
+        )
+    }
+
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
+
+  const notesToShow = showAll 
+    ? notes 
+    : notes.filter((note) => note.important === true)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      <Notification message={errorMessage} />
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}</button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note 
+            key={note.id}
+            note={note}
+            toggleImportance={() => {toggleImportance(note.id)}}  
+            />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
