d09d307036685e1b96a2cc66b16f04bb0e9f80ce
azevedo7 <jazevedo0406@gmail.com>
2024-08-20 08:19:26 +0100

    Blog List: 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false) 
+
+  const blogStyle = {
+    border: '1px solid black',
+    padding: 5,
+    margin: '5px 0px',
+
+  }
+
+  const toggleView = () => {setView(!view)}
+
+  if(!view){
+    return(
+      <div style={blogStyle}>
+        {blog.title}, {blog.author}
+        <button onClick={toggleView}>view</button>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleView}>hide</button>
+      </div>
+      <div><a href={blog.url}>{blog.url}</a></div>
+      <div>likes: {blog.likes}</div>
+      <div>{blog.author}</div>
+    </div>
+  )
+}
 
 export default Blog
