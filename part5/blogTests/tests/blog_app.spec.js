42712fa07b75ec89243e807677017b3b7f209574
azevedo7 <jazevedo0406@gmail.com>
2024-08-23 18:59:33 +0100

    Blog list: 5.22

--- part5/blogTests/tests/blog_app.spec.js
+++ part5/blogTests/tests/blog_app.spec.js
@@ -61,4 +61,41 @@ describe('Blog app', () => {
             page.on('dialog', dialog => dialog.accept());
         })
     })
+
+    describe('when multiple users', () => {
+        beforeEach(async ({ page, request}) => {
+            await request.post('/api/users/', {
+                data: {
+                    username: 'user1',
+                    name: 'user1',
+                    password: 'password'
+                }
+            })
+            await request.post('/api/users/', {
+                data: {
+                    username: 'user2',
+                    name: 'user2',
+                    password: 'password'
+                }
+            })
+
+            await page.goto('/')
+        })
+
+        test.only('only the user who added the blog can delete it', async ({ page }) => {
+            // user1 will add a blog and user2 will open the blog and try to remove it
+            await loginWith(page, 'user1', 'password')    
+            await addBlog(page, 'test note', 'test author', 'http://example.com')
+            await page.getByText('test note, test author').waitFor()
+
+            // logout
+            await page.getByRole('button', { name: 'logout' }).click()
+
+            await loginWith(page, 'user2', 'password')
+            await page.getByRole('button', { name: 'view' }).waitFor()
+            await page.getByRole('button', { name: 'view' }).click()
+
+            await expect(page.getByText('delete')).not.toBeVisible()
+        })
+    })
 })
