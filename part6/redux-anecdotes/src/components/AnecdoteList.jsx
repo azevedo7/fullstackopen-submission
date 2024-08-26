7ddbea64528a1cecee952c9625dedd1dcb5301bf
azevedo7 <jazevedo0406@gmail.com>
2024-08-26 22:02:38 +0100

    Anecdotes: 6.13

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification } from "../reducers/notificationReducer"
 
 const Anecdote = ({ anecdote, handleClick}) => {
     return(
@@ -26,14 +27,18 @@ const AnecdoteList = () => {
 
     const dispatch = useDispatch()
 
-    const vote = (id) => {
-        dispatch(voteAnecdote(id))
+    const vote = (anecdote) => {
+        dispatch(voteAnecdote(anecdote.id))
+        dispatch(setNotification(`you voted '${anecdote.content}'`))
+        setTimeout(() => {
+            dispatch(setNotification(''))
+        }, 5000)
     } 
 
     return (
         <div>
             {sortedAnecdotes.map(anecdote =>
-                    <Anecdote key={anecdote.id} anecdote={anecdote} handleClick={() => vote(anecdote.id)} />
+                    <Anecdote key={anecdote.id} anecdote={anecdote} handleClick={() => vote(anecdote)} />
                 )}
         </div>
 
