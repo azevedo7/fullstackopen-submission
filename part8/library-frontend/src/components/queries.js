5bca202e3003e018c0676cf53122eeadcf488315
azevedo7 <jazevedo0406@gmail.com>
2024-09-06 16:10:53 +0100

    Library-Frontend: 8.10

--- part8/library-frontend/src/components/queries.js
+++ part8/library-frontend/src/components/queries.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client'
+import { gql } from "@apollo/client"
 
 export const ALL_AUTHORS = gql`
   query {
@@ -11,13 +11,34 @@ export const ALL_AUTHORS = gql`
 `
 
 export const ALL_BOOKS = gql`
-    query {
-        allBooks {
-            author,
-            title,
-            genres,
-            published,
-            id
-        }
+  query {
+    allBooks {
+      author
+      title
+      genres
+      published
+      id
     }
+  }
+`
+
+export const ADD_BOOK = gql`
+  mutation addBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+        title
+        author
+        published
+        genres
+    }
+  }
 `
