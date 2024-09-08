78c895548a87900fbb58e0bd243c74f727a72c95
azevedo7 <jazevedo0406@gmail.com>
2024-09-08 12:01:05 +0100

    Library-Backend: 8.14

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,103 +1,100 @@
-const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
-const { GraphQLError } = require('graphql')
-const mongoose = require('mongoose')
-const Author = require('./models/author')
-const Book = require('./models/book')
+const { ApolloServer } = require("@apollo/server")
+const { startStandaloneServer } = require("@apollo/server/standalone")
+const { GraphQLError } = require("graphql")
+const mongoose = require("mongoose")
+const Author = require("./models/author")
+const Book = require("./models/book")
 
-require('dotenv').config()
+require("dotenv").config()
 
 const MONGODB_URI = process.env.MONGODB_URI
 
-console.log('connecting to', MONGODB_URI)
+console.log("connecting to", MONGODB_URI)
 
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log("connected to MongoDB")
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
+    console.log("error connection to MongoDB:", error.message)
   })
-
-
-
 
 let authors = [
   {
-    name: 'Robert Martin',
+    name: "Robert Martin",
     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
     born: 1952,
   },
   {
-    name: 'Martin Fowler',
+    name: "Martin Fowler",
     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
+    born: 1963,
   },
   {
-    name: 'Fyodor Dostoevsky',
+    name: "Fyodor Dostoevsky",
     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
+    born: 1821,
   },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
+  {
+    name: "Joshua Kerievsky", // birthyear not known
     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
   },
-  { 
-    name: 'Sandi Metz', // birthyear not known
+  {
+    name: "Sandi Metz", // birthyear not known
     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
   },
 ]
 
 let books = [
   {
-    title: 'Clean Code',
+    title: "Clean Code",
     published: 2008,
-    author: 'Robert Martin',
+    author: "Robert Martin",
     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
+    genres: ["refactoring"],
   },
   {
-    title: 'Agile software development',
+    title: "Agile software development",
     published: 2002,
-    author: 'Robert Martin',
+    author: "Robert Martin",
     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
+    genres: ["agile", "patterns", "design"],
   },
   {
-    title: 'Refactoring, edition 2',
+    title: "Refactoring, edition 2",
     published: 2018,
-    author: 'Martin Fowler',
+    author: "Martin Fowler",
     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
+    genres: ["refactoring"],
   },
   {
-    title: 'Refactoring to patterns',
+    title: "Refactoring to patterns",
     published: 2008,
-    author: 'Joshua Kerievsky',
+    author: "Joshua Kerievsky",
     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
+    genres: ["refactoring", "patterns"],
+  },
   {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
     published: 2012,
-    author: 'Sandi Metz',
+    author: "Sandi Metz",
     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
+    genres: ["refactoring", "design"],
   },
   {
-    title: 'Crime and punishment',
+    title: "Crime and punishment",
     published: 1866,
-    author: 'Fyodor Dostoevsky',
+    author: "Fyodor Dostoevsky",
     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
+    genres: ["classic", "crime"],
   },
   {
-    title: 'Demons',
+    title: "Demons",
     published: 1872,
-    author: 'Fyodor Dostoevsky',
+    author: "Fyodor Dostoevsky",
     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
+    genres: ["classic", "revolution"],
   },
 ]
 
@@ -140,78 +137,80 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
-        let updatedBooks = Book.find({})
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      const filters = {}
 
-        if(args.author) {
-            const byAuthor = (book) => {return book.author == args.author}
-            updatedBooks = updatedBooks.filter(byAuthor)
-        }
-        
-        if(args.genre) {
-            const byGenre = (book) => {return book.genres.includes(args.genre)}
-            updatedBooks = updatedBooks.filter(byGenre)
-        }
-        
-        return updatedBooks 
+      // if (args.author) {
+      //   const author = await Author.findOne({ name: args.author })
+      //   filters.author = args.author._id
+      // }
+
+      if (args.genre) {
+        filters.genres = args.genre
+      }
+
+      let updatedBooks = await Book.find(filters).populate("author")
+      return updatedBooks
     },
-    allAuthors: () => authors,
+    allAuthors: async () => Author.find({}),
   },
   Mutation: {
-    addBook: async(root, args) => {
-        let author = await Author.findOne({ name: args.author });
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
 
-        if(!author) {
-            author = new Author({ name: args.author })
-            try{
-              await author.save()
-            } catch(error) {
-              throw new GraphQLError("Creating author failed", {
-                extensions: {
-                  code: "INTERNAL_SERVER_ERROR",
-                  http: {
-                    status: 500
-                  },
-                  error
-                }  
-              })
-            }
+      if (!author) {
+        author = new Author({ name: args.author })
+        try {
+          await author.save()
+        } catch (error) {
+          throw new GraphQLError("Creating author failed", {
+            extensions: {
+              code: "INTERNAL_SERVER_ERROR",
+              http: {
+                status: 500,
+              },
+              error,
+            },
+          })
         }
-        
-        const book = new Book({...args, author})
+      }
 
-        try{
-            await book.save()
-        } catch(error) {
-            throw new GraphQLError("Creating book failed", {
-                extensions: {
-                  code: "INTERNAL_SERVER_ERROR",
-                  http: {
-                    status: 500
-                  },
-                  error
-                }  
-              })
-        }
+      const book = new Book({ ...args, author })
 
-        return book
+      try {
+        await book.save()
+      } catch (error) {
+        throw new GraphQLError("Creating book failed", {
+          extensions: {
+            code: "INTERNAL_SERVER_ERROR",
+            http: {
+              status: 500,
+            },
+            error,
+          },
+        })
+      }
+
+      return book
     },
     editAuthor: (root, args) => {
-        let newAuthor = authors.find(a => a.name === args.name)
-        if(!newAuthor) { return null }
+      let newAuthor = authors.find((a) => a.name === args.name)
+      if (!newAuthor) {
+        return null
+      }
 
-        newAuthor.born = args.setBornTo
-        authors = authors.map(a => a.id === newAuthor.id ? newAuthor : a) 
-        return newAuthor
-    }
+      newAuthor.born = args.setBornTo
+      authors = authors.map((a) => (a.id === newAuthor.id ? newAuthor : a))
+      return newAuthor
+    },
   },
   Author: {
     bookCount: (root) => {
-        return books.filter((book) => book.author === root.name).length
-    }
-  } 
+      return books.filter((book) => book.author === root.name).length
+    },
+  },
 }
 
 const server = new ApolloServer({
