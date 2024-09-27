0fbcc0d658a2eef1558b5571a9660e0bc83a6e92
azevedo7 <jazevedo0406@gmail.com>
2024-09-27 16:56:51 +0100

    TypeScript: 9.5

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -4,7 +4,7 @@ interface ParseValues {
 }
 
 const calculateBmi = (height: number, weight: number): string => {
-    const bmi = weight / (height/100)**2
+    const bmi = weight / (height / 100) ** 2
     switch (true) {
         case bmi < 16.0:
             return 'Underweight (Severe thinness)';
@@ -28,15 +28,44 @@ const calculateBmi = (height: number, weight: number): string => {
 }
 
 const parseArgumentsBmi = (argv: string[]): ParseValues => {
-    if(argv.length > 4) throw new Error("Too many arguments");
-    if(argv.length < 4) throw new Error("Too few arguments");
+    if (argv.length > 4) throw new Error("Too many arguments");
+    if (argv.length < 4) throw new Error("Too few arguments");
 
-    if(!isNaN(Number(argv[2])) && !isNaN(Number(argv[3]))){
+    if (!isNaN(Number(argv[2])) && !isNaN(Number(argv[3]))) {
         return {
             height: Number(argv[2]),
             weight: Number(argv[3])
         }
     } else throw new Error("Only input numbers (heigh weigth)");
 }
-const { height, weight } = parseArgumentsBmi(process.argv)
-console.log(calculateBmi(height, weight))
+
+const queryArguments = (height: string, weight: string): ParseValues => {
+    if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
+        return {
+            height: Number(height),
+            weight: Number(weight)
+        }
+    } else throw new Error("Only input numbers (heigh weigth)");
+}
+
+
+const Bmi = (h: string, w: string): {weight: number, height: number, bmi: string} | { error: string } => {
+    try{
+        const { height, weight } = queryArguments(h, w);
+        const bmiresult = calculateBmi(height, weight);
+        return {
+            weight,
+            height,
+            bmi: bmiresult
+        }
+    } catch(e: unknown) {
+        return {error: 'malformated parameters'}
+    }
+}
+
+if (require.main === module) {
+    const { height, weight } = parseArgumentsBmi(process.argv);
+    console.log(calculateBmi(height, weight));
+}
+
+export default Bmi;
