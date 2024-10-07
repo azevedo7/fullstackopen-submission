efcb1f41c1b2b5cd1e3debe0c2c6807451abb84c
azevedo7 <jazevedo0406@gmail.com>
2024-10-07 23:21:16 +0100

    Patientor: 9.24

--- part9/patientor-frontend/src/components/PatientDetailsPage/index.tsx
+++ part9/patientor-frontend/src/components/PatientDetailsPage/index.tsx
@@ -3,7 +3,7 @@ import MaleIcon from '@mui/icons-material/Male';
 import { useParams } from 'react-router-dom';
 import patientService from "../../services/patients";
 import { useEffect, useState } from 'react';
-import { Patient } from "../../types";
+import { Patient, Entry } from "../../types";
 
 const PatientDetailsPage = () => {
     const [patient, setPatient] = useState<Patient>();
@@ -33,8 +33,33 @@ const PatientDetailsPage = () => {
             <h2>{patient.name} {patient.gender === 'male' ? <MaleIcon /> : <FemaleIcon />}</h2>
             <div>ssn: {patient.ssn}</div>
             <div>occupation: {patient.occupation}</div>
+            <Entries entries={patient.entries} />
         </div>
     );
+};
+
+const Entries = ({ entries }: { entries: Entry[] }) =>{
+    if(!entries || entries.length === 0) {
+        return null;
+    }
+
+    return (
+        <div>
+            <h3>entries</h3>
+            {entries.map(entry => (
+                <div>
+                    <p>{entry.date} {entry.description}</p>
+                    <ul>
+                        {entry.diagnosisCodes?.map(code => (
+                            <li key={code}>
+                                {code}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    )
 };
 
 export default PatientDetailsPage;
