afbeb519347c3a9d38922fa5209a2b326fe33df4
azevedo7 <jazevedo0406@gmail.com>
2024-10-07 23:11:27 +0100

    Patientor: 9.23

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { newPatientSchema } from "./utils";
 
-export interface Diagnoses {
+export interface Diagnosis {
     code: string,
     name: string,
     latin?: string
@@ -28,8 +28,52 @@ export type NewPatientEntry = z.infer<typeof newPatientSchema>;
 
 export type PatientNonSensitiveInfo = Omit<Patient, "ssn">;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry{
+interface BaseEntry{
+    id: string,
+    description: string,
+    date: string,
+    specialist: string,
+    diagnosisCodes?: Diagnosis['code'][];
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck",
+    healthCheckRating: HealthCheckRating
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare",
+    employerName: string,
+    sickLeave?: {
+        startDate: string,
+        endDate: string
+    }
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital",
+    discharge: {
+        date: string,
+        criteria: string
+    }
+}
+
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+// Define Entry without the 'id' property
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
